375a78fadd054aabe79b79c35fb997e425992d40
deezy13 <deezy13@gmail.com>
2019-09-22 03:57:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,12 @@
 def nyc_pigeon_organizer(data)
  
 final_list = Hash.new
-   data.each |level_one, other_levels|
+   data.each do |level_one, other_levels|
    other_levels.each do | properties, names |
      names.each do |name|
-       final_list[name] = {
+       final_list[name] = {:color =>[], :gender => [], :lives =>[]
        
-     
-   
- 
+     end 
+ end 
 end
+end 
