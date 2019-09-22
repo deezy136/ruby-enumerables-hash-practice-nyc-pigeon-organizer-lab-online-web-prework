250e2bbb63bc70cc1ba10493439e017aa023c521
deezy13 <deezy13@gmail.com>
2019-09-22 18:30:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,7 +24,7 @@ pigeon_keys = final_list.keys
   
   data[:gender].each do |gender, type|
     type.each do |bird_name|
-      x.each do |item|
+      pigeon_keys.each do |item|
         if bird_name === item
           final_list[item][:gender] << gender.to_s
         end 
@@ -33,7 +33,7 @@ pigeon_keys = final_list.keys
   end 
   data[:lives].each do |location, name|
     name.each do |bird_name|
-      x.each do |item|
+      pigeon_keys.each do |item|
         if bird_name === item
           final_list[item][:lives] << location
         end 
